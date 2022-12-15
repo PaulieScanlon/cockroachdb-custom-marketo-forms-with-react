ccc5ae8936a3ff8aa3f7873f9b0fa58152c37b44
Paul Scanlon <paul@crlMBP-NW016XCPYWMjk3.local>
2022-12-15 15:01:18 -0800

    refactor: add getElementId to marketo hook

--- hooks/use-marketo.js
+++ hooks/use-marketo.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react'
 
-const baseUrl = `//${process.env.NEXT_PUBLIC_BASE_URL}`
-const munchkinId = process.env.NEXT_PUBLIC_MUNCHKIN_ID
+const marketoScriptId = 'marketo-forms2'
 
 const loadMarketoScript = (setScriptLoaded) => {
   if (window.MktoForms2) return setScriptLoaded(true)
+
   const script = document.createElement('script')
   script.defer = true
   script.onload = () => (window?.MktoForms2 ? setScriptLoaded(true) : null)
   script.src = `//${process.env.NEXT_PUBLIC_BASE_URL}/js/forms2/js/forms2.min.js`
-  document.head.appendChild(script)
+  script.setAttribute('id', marketoScriptId)
+
+  if (!document.getElementById(marketoScriptId)) {
+    document.head.appendChild(script)
+  }
 }
 
 const useMarketo = ({ formId, callback }) => {
@@ -19,7 +23,12 @@ const useMarketo = ({ formId, callback }) => {
   useEffect(() => {
     if (scriptLoaded) {
       if (!formLoaded) {
-        MktoForms2.loadForm(baseUrl, munchkinId, formId, callback)
+        MktoForms2.loadForm(
+          `//${process.env.NEXT_PUBLIC_BASE_URL}`,
+          process.env.NEXT_PUBLIC_MUNCHKIN_ID,
+          formId,
+          callback
+        )
         setFormLoaded(true)
       }
     } else {
